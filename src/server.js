e97ef0dda0a03056c67b48b6eac069500cdf4525
cheonyeji <ssaltuck@gmail.com>
2022-08-22 19:04:28 +0900

    add body-parser (for req.body undefined)

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import express from "express";
 import morgan from "morgan";
 import deliveryRouter from "./routers/delivery/deliveryRouter";
@@ -9,6 +10,7 @@ const app = express();
 const logger = morgan("dev");
 
 app.use(logger);
+app.use(bodyParser.json());
 
 app.use("/", globalRouter);
 app.use("/user", userRouter);
