7fe0e9d1ac67deda613b256838bfc9974c87af92
cheonyeji <ssaltuck@gmail.com>
2022-08-22 13:20:21 +0900

    Add db connection code

--- src/db.js
+++ src/db.js
@@ -1,15 +1,51 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+export const pool = mysql.createPool({
   host: process.env.AWS_DB_HOST,
   user: process.env.AWS_DB_USER,
   password: process.env.AWS_DB_PASSWORD,
   port: process.env.AWS_DB_PORT,
-  database: "test",
+  database: "kurylyDB",
+  connectionLimit: 30,
+  enableKeepAlive: true,
 });
+
+export async function getConnectionPool(callback) {
+  const connection = await pool.getConnection(async (err, conn) => {
+    if (!err) {
+      return conn;
+    } else {
+      console.log("db connection err", err);
+      throw err;
+    }
+  });
+  console.log("✅ Connected to DB 🎉");
+  callback(connection);
+}
+
+// const getOrderItemQuery = async () => {
+//   try {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     try {
+//       let [rows] = await connection.query("SELECT * FROM `order`");
+//       connection.release();
+//       console.log(rows);
+//       return rows;
+//     } catch (err) {
+//       console.log("Query Error", err);
+//       connection.release();
+//       return false;
+//     }
+//   } catch (err) {
+//     console.log("DB error");
+//     return false;
+//   }
+// };
+
+// getOrderItemQuery();
 
 // connection.connect(function (err) {
 //   if (err) {
@@ -18,8 +54,8 @@ const connection = mysql.createConnection({
 //   console.log("✅ Connected to DB 🎉");
 // });
 
-connection.query("SELECT * FROM fruit", function (err, rows, fields) {
-  console.log(rows); // 결과를 출력합니다!
-});
+// connection.query("SELECT * FROM fruit", function (err, rows, fields) {
+//   console.log(rows); // 결과를 출력합니다!
+// });
 
 // connection.end();
