265771866fb662b763e7cb3860cb572092200c2a
cheonyeji <ssaltuck@gmail.com>
2022-08-23 15:49:38 +0900

    바뀐 사양에 따라 배송지연, 배송완료 나뉘어서 라우팅

--- src/routers/delivery/msgRouter.js
+++ src/routers/delivery/msgRouter.js
@@ -2,18 +2,16 @@ import express from "express";
 import {
   getDeliveryDones,
   getDeliveryToDos,
-  getDeliveryToDoItem,
-  updateDeliveryToDoItem,
+  updateDeliveryToDoItemToStatusTwo,
+  updateDeliveryToDoItemToStatusThree,
 } from "../../controllers/msgController";
 
 const msgRouter = express.Router();
 
 msgRouter.get("/todo", getDeliveryToDos);
 
-msgRouter
-  .route("/todo/:trackingnum")
-  .get(getDeliveryToDoItem)
-  .post(updateDeliveryToDoItem);
+msgRouter.post("/todo/2/:trackingnum", updateDeliveryToDoItemToStatusTwo);
+msgRouter.post("/todo/3/:trackingnum", updateDeliveryToDoItemToStatusThree);
 
 msgRouter.get("/done", getDeliveryDones);
 
