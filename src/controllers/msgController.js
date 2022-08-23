8349a1b10e89c80d63acd4297341ecb9074f55ea
cheonyeji <ssaltuck@gmail.com>
2022-08-23 15:48:26 +0900

    미배송 운송장 관련 업데이트

--- src/controllers/msgController.js
+++ src/controllers/msgController.js
@@ -1,14 +1,89 @@
+import { getConnectionPool } from "../database/db";
+import {
+  getDeliveryDonesQuery,
+  getDeliveryToDosQuery,
+  updateDeliveryToDoItemToStatusThreeQuery,
+  updateDeliveryToDoItemToStatusTwoQuery,
+} from "../database/query";
+
 export const getDeliveryToDos = (req, res) => {
-  return res.send("msg 할일이 많아요~");
+  getConnectionPool(async (connection) => {
+    try {
+      let [rows] = await connection.query(getDeliveryToDosQuery);
+      connection.release();
+      return res.send(rows);
+    } catch (err) {
+      console.log("Query Error", err);
+      connection.release();
+      return res.send(err);
+    }
+  });
 };
 export const getDeliveryDones = (req, res) => {
-  return res.send("msg 완료된 일을 늘려요~");
+  getConnectionPool(async (connection) => {
+    try {
+      let [rows] = await connection.query(getDeliveryDonesQuery);
+      connection.release();
+      return res.send(rows);
+    } catch (err) {
+      console.log("Query Error", err);
+      connection.release();
+      return res.send(err);
+    }
+  });
 };
 
-export const getDeliveryToDoItem = (req, res) => {
-  return res.send(`${req.params.trackingnum}을 조회합니다.`);
+export const updateDeliveryToDoItemToStatusTwo = (req, res) => {
+  // 채팅도 저장해야 되고 배송지연으로 처리도 해야함!!!!
+  const { text, img_uri, is_first_msg } = req.body;
+  const queryParams = [
+    req.params.trackingnum,
+    text,
+    req.params.trackingnum,
+    img_uri,
+    is_first_msg,
+    req.params.trackingnum,
+  ];
+  getConnectionPool(async (connection) => {
+    try {
+      let [rows] = await connection.query(
+        updateDeliveryToDoItemToStatusTwoQuery,
+        queryParams
+      );
+      connection.release();
+      return res.send("DB update success");
+    } catch (err) {
+      console.log("Query Error", err);
+      connection.release();
+      return res.send(err);
+    }
+  });
 };
 
-export const updateDeliveryToDoItem = (req, res) => {
-  return res.send(`${req.params.trackingnum}을 배송완료 처리합니다.`);
+export const updateDeliveryToDoItemToStatusThree = (req, res) => {
+  // 채팅도 저장해야 되고 배송완료로 처리도 해야함!!!!
+  const { text, img_uri, is_first_msg } = req.body;
+  const queryParams = [
+    req.params.trackingnum,
+    text,
+    req.params.trackingnum,
+    img_uri,
+    is_first_msg,
+    req.params.trackingnum,
+    req.params.trackingnum,
+  ];
+  getConnectionPool(async (connection) => {
+    try {
+      await connection.query(
+        updateDeliveryToDoItemToStatusThreeQuery,
+        queryParams
+      );
+      connection.release();
+      return res.send("DB update success");
+    } catch (err) {
+      console.log("Query Error", err);
+      connection.release();
+      return res.send(err);
+    }
+  });
 };
